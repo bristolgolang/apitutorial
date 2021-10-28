6a449605523ac0694a69aa2bd6f2a89aa6908846
AJT <alextrelore@gmail.com>
2021-10-28 19:34:16 +0100

    Uses go:embed to embed the static files

--- lesson-webapp/main.go
+++ lesson-webapp/main.go
@@ -1,18 +1,22 @@
 package main
 
 import (
+	"embed"
 	"fmt"
-	"html/template"
 	"log"
 	"math/rand"
 	"net/http"
+	"text/template"
 	"time"
 )
 
 const port = "8080"
 
-var todoTmpl = template.Must(template.ParseFiles("lesson-webapp/static/todoPage.html"))
-var quoteTmpl = template.Must(template.ParseFiles("lesson-webapp/static/quote.html"))
+//go:embed static/*
+var staticFiles embed.FS
+
+var todoFile = template.Must(template.ParseFS(staticFiles, "static/todoPage.html"))
+var quoteFile = template.Must(template.ParseFS(staticFiles, "static/quote.html"))
 
 // TodoPageData is the data structue for the todo page
 type TodoPageData struct {
@@ -44,31 +48,32 @@ func todoPage(w http.ResponseWriter, r *http.Request) {
 	data := TodoPageData{
 		Title: "My Todo Page",
 		Items: []TodoItem{
-			TodoItem{
+			{
 				Title: "Item 1",
 				Done:  false,
 			},
-			TodoItem{
+			{
 				Title: "Item 2",
 				Done:  true,
 			},
 		},
 	}
-	todoTmpl.Execute(w, data)
+
+	todoFile.Execute(w, data)
 }
 
 func quote(w http.ResponseWriter, r *http.Request) {
 	quotes := []Quote{
-		Quote{Text: "Simplicity is prerequisite for reliability.", Author: "Edsger W. Dijkstra"},
-		Quote{Text: "Good naming is like a good joke. If you have to explain it, it’s not funny. ", Author: "Dave Cheney"},
-		Quote{Text: "Don’t comment bad code — rewrite it ", Author: "Brian Kernighan "},
-		Quote{Text: "APIs should be easy to use and hard to misuse.", Author: "Josh Bloch"},
-		Quote{Text: "Readability is essential for maintainability.", Author: "Mark Reinhold"},
-		Quote{Text: "The most important skill for a programmer is the ability to effectively communicate ideas.", Author: "Gastón Jorquera"},
+		{Text: "Simplicity is prerequisite for reliability.", Author: "Edsger W. Dijkstra"},
+		{Text: "Good naming is like a good joke. If you have to explain it, it’s not funny. ", Author: "Dave Cheney"},
+		{Text: "Don’t comment bad code — rewrite it ", Author: "Brian Kernighan "},
+		{Text: "APIs should be easy to use and hard to misuse.", Author: "Josh Bloch"},
+		{Text: "Readability is essential for maintainability.", Author: "Mark Reinhold"},
+		{Text: "The most important skill for a programmer is the ability to effectively communicate ideas.", Author: "Gastón Jorquera"},
 	}
 
 	rand.Seed(time.Now().Unix())
 	quote := quotes[rand.Intn(len(quotes))]
 
-	quoteTmpl.Execute(w, quote)
+	quoteFile.Execute(w, quote)
 }
